8fdd5199c4f80c238b6d39545465ef6ad2637869
Prasanna Sabhahit <prasannasabhahit24@gmail.com>
2026-02-11 22:25:46 +0530

    Time: 3 ms (32.89%), Space: 8.8 MB (35.87%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        int cnt=0;
+        for(int i=1;i<=n;i++){
+               cnt++;
+              n=n-i;
+        }
+        return cnt;
+    }
+};
