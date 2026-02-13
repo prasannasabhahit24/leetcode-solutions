37ddd38137563a639d2414165b66447f4ef21474
Prasanna Sabhahit <prasannasabhahit24@gmail.com>
2026-02-13 22:29:14 +0530

    Time: 0 ms (100%), Space: 8.6 MB (77.3%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        if(num == 0) return 0;
+        
+        
+        while(num >=10) {           //repeat tillle single digit
+            int sum=0;
+             while(num != 0) {
+                int d=num%10;
+                sum+=d;
+                 num/=10;
+             }
+             num=sum;
+        }
+        return num;
+    }
+};
