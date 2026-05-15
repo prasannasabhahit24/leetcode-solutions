00002f31c06a458bc7eb7200a8a51086d5673c80
Prasanna Sabhahit <prasannasabhahit24@gmail.com>
2026-05-15 23:44:19 +0530

    Time: 3 ms (37.13%), Space: 42.1 MB (31.26%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -5,17 +5,22 @@ public:
         int n=nums.size();
 
          vector<int> ans(n);
-        ans[0]=1;   //since it is product
-         
-        for(int i=1;i<n;i++){
-            ans[i]=ans[i-1]*nums[i-1];
-        }
+        vector<int> suffix(n,1);
+       vector<int> pref(n,1);
 
-        int right=1;
-        for(int j=n-1;j>=0;j--){
-            ans[j]=right*ans[j];
-            right=right*nums[j];
-        }
-        return ans;
+
+       for(int i=1;i<n;i++){
+        pref[i]=pref[i-1]*nums[i-1];
+       }
+
+       for(int i=n-2;i>=0;i--){
+        suffix[i]=suffix[i+1]*nums[i+1];
+       }
+
+       for(int i=0;i<n;i++){
+        ans[i]=pref[i]*suffix[i];
+       }
+
+       return ans;
     }
 };
