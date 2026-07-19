7eddd7ef0af58049afaeb96b881a26451b336eab
Prasanna Sabhahit <prasannasabhahit24@gmail.com>
2026-07-19 22:56:22 +0530

    Time: 1 ms (21.34%), Space: 8.9 MB (17.14%) - LeetHub

---
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string smallestSubsequence(string s) {
+         int n=s.length();
+        unordered_map<char,int> mp;
+
+        
+     
+        for(char c:s){
+            mp[c]++;
+        }
+
+           unordered_map<char,bool> visited;
+           string ans="";
+        for(char c:s){
+             mp[c]--;
+           if(visited[c]==true){
+            continue;
+           }
+             //if we want ro remove it must be lesser in lexigraphical order 
+             while(!ans.empty() && c<ans.back() && mp[ans.back()]>0){
+                visited[ans.back()]=false;
+                ans.pop_back();
+             }
+             ans.push_back(c);
+              visited[c]=true;
+        }
+        return ans;
+    }
+};
