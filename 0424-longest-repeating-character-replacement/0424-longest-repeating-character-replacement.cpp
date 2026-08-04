46871f359e4a0d6ae07c82dbf5a8196bd8185f32
Prasanna Sabhahit <prasannasabhahit24@gmail.com>
2026-08-04 22:48:56 +0530

    Time: 2 ms (71.78%), Space: 10.9 MB (42.2%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int n=s.length();
+
+        int hash[26]={0};
+        int maxfreq=0;
+        int maxlen=0;
+        int l=0;
+        int r=0;
+        while(r < n){
+            hash[s[r]-'A']++;
+
+            maxfreq=max(maxfreq,hash[s[r]-'A']);
+
+            if((r-l+1)-maxfreq > k){
+                hash[s[l]-'A']--;
+
+                l++;
+            }
+
+            if((r-l+1)-maxfreq <= k){
+                maxlen=max(maxlen,r-l+1);
+            }
+            r++;
+
+        }
+
+        return maxlen;
+    }
+};
