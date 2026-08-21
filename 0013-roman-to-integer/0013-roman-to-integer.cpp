e733d61a5600ebc4011204e147c8c36a014a6b4b
Prasanna Sabhahit <prasannasabhahit24@gmail.com>
2026-08-21 22:43:21 +0530

    Time: 13 ms (13.84%), Space: 13.3 MB (38.21%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int ans=0;
+
+        unordered_map<char,int> roman={
+            {'I',1}, {'V',5}, {'X',10}, {'L',50}, {'C',100}, {'D',500}, {'M',1000}
+        };
+
+        for(int i=0;i<s.size()-1;i++){
+            if(roman[s[i]] < roman[s[i+1]]){
+                ans-=roman[s[i]];
+            }
+            else {
+                 ans+=roman[s[i]];
+            }
+        }
+        return ans+roman[s.back()];   // finally add the value of the last character
+        // (because loop stops before the last one)
+    }
+};
